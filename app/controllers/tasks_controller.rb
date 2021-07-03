62abdc1827cf61fd3ca20775036542f7ec79a9a9
Yui Kondo <yuikondobook@gmail.com>
2021-07-03 17:13:58 +0900

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,48 @@
 class TasksController < ApplicationController
     # Read
-    # Create
-    # Update
-    # Destroy
+    # See all tasks
+    def index
+        @tasks = Task.all
+    end
+    # See details about one task
+    def show
+        @task = Task.find(params[:id])
+    end
+    # Create a restaurant
+    def new
+        @task = Task.new # needed to instantiate the form_for
+    end
+
+    def create
+        @task = Task.new(task_params) 
+        # DIFF taks_params & params[:task]
+        @task.save
+        # Will raise ActiveModel::ForbiddenAttributesError
+        redirect_to task_path(@task)
+    end
+
+    # Update a restaurant
+    def edit
+        @task = Task.find(params[:id])
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        @task.update(task_params)
+        redirect_to task_path(@task)
+    end
+    # Destroy a restaurant
+
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+
+        redirect_to tasks_path
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:title, :details, :completed)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Read
+  # get "tasks", to: "tasks#index"
+  # get "tasks/new", to: "tasks#new"
+  # # Create
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # post "tasks", to: "tasks#create"
+
+  # # # Update
+  # # # GET the HTML form (pre-filled with restaurant attributes) for editing = 1 request
+  # # PATCH the parameters to update an existing restaurant = 1 request
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+
+  # # Delete
+  # delete "tasks/:id", to: "tasks#destroy"
+
 end
